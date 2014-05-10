0bf6d7be4d7acf0518105dba4a0cadde9d0dd50c
Jasper Wallace <jasper@pointless.net>
2014-05-10 22:24:33 +0100

    make long messages scroll

--- SixteenBoard.py
+++ SixteenBoard.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
 import serial, re, time, threading, Queue
 from sixteensegfont import font
 
@@ -26,8 +28,11 @@ class SixteenWorker():
             # ideally these would go to irc or something.
             raise RuntimeError('unable to open serial port?')
         self.lastmsg = ''
-        self.clear()
         self.queue = queue
+        self.scroller = None
+        self.stop = threading.Event()
+
+        self.clear()
 
     def run(self):
         while True:
@@ -43,28 +48,58 @@ class SixteenWorker():
             self.port.write("w" + mangle(font[c]))
             time.sleep(0.5)
 
+    def scroll_message(self, msg, stop):
+        pos = 0
+        # add a space on the end to the wrap around works better
+        if msg[-1] != " ":
+            msg += " "
+
+        while True:
+            if stop.isSet():
+                break
+            self.port.write("w" + mangle(font[msg[pos]]))
+            if stop.wait(0.5):
+                break
+            pos += 1
+            if pos == len(msg):
+                pos = 0
+
     def clear(self):
-        self.port.write("r")
+        if self.scroller:
+            self.stop.set()
+            self.scroller.join()
+            self.stop.clear()
+        # "r" dosn't work properly, clocked too fast?!?
+#        self.port.write("r")
+        for i in range(0,7):
+            self.port.write("w" + mangle(font[" "]))
+            time.sleep(0.25)
+#        self.port.write("r")
 
     def display(self, msg, permanent=True):
         assert isinstance(msg, unicode)
         if len(msg) == 0:
             self.clear()
             return
-        msg = msg[:32] # guess
 #        msg = msg.replace(u'\xa3', '\x1f') # Fix up for pound
+        # XXX no £ sign in the font :(
 
         if not re.match('^[ -~]*$', msg):
             raise ValueError('Standard ASCII only, please')
         
         self.clear()
-        self.message(msg)
+        if len(msg) <= 8:
+            self.message(msg)
+        else:
+            self.scroller = threading.Thread(name="sixteen_scroll", target=self.scroll_message, kwargs={"msg": msg, "stop": self.stop})
+            self.scroller.setDaemon(True)
+            self.scroller.start()
 
         if permanent:
             self.lastmsg = msg
 
     def restore(self):
-        self.message(str(self.lastmsg))
+        self.display(str(self.lastmsg), True)
 
 class SixteenBoard(object):
     def __init__(self, serialport):
